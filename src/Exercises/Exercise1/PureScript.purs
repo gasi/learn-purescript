5241d37e54268ac47542b941f4eae0ec8d188222
Daniel Gasienica <daniel@gasienica.ch>
2019-05-11 12:59:50 -0400

    Document `Maybe`

--- src/Exercises/Exercise1/PureScript.purs
+++ src/Exercises/Exercise1/PureScript.purs
@@ -9,6 +9,18 @@ import Data.Maybe (Maybe(..))
 
 -- TODO: Implement `add2` to do the same as the JavaScript implementation
 -- in `JavaScript.js` file:
+--
+-- Reminder:
+--
+--    data Maybe a  -- `Maybe`: Name of type
+--      = Nothing   -- `Nothing`: Constructor for when we don’t have a value
+--      | Just a    -- `Just`: Constructor for when we do have a value.
+--                     The `a` in `Just a` refers to the `a` in `Maybe a`.
+--                     It stands for a type, e.g., `Int`, `String`, etc.
+--                     This will let you describe different kinds of `Maybe`
+--                     without having to write new code, e.g. `Maybe Int`,
+--                     `Maybe String`, etc.
+--
 add2 :: Maybe Int -> Maybe Int
 add2 _ = Nothing
 
